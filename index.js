c0d7bc08c2241c74a808017788b4df9067efb3f1
Guillermo Quinteros <gu.quinteros@gmail.com>
2022-07-24 09:34:31 -0400

    feat: add filtered services

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ app.get('/snes', (req, res) => {
     res.json(snes);
 })
 
+app.get('/snes/USA', (req, res) => {
+    const games = snes.filter(game => game.region === 'NA');
+    res.json(games);
+})
+
+app.get('/snes/EUR', (req, res) => {
+    const games = snes.filter(game => game.region === 'EU');
+    res.json(games);
+})
+
+app.get('/snes/JAP', (req, res) => {
+    const games = snes.filter(game => game.region === 'JP');
+    res.json(games);
+})
+
+app.use((req, res, next) => {
+    res.status(404);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
