22c1b99b51ebafb8f463f675b41045c926256c79
Guillermo Quinteros <gu.quinteros@gmail.com>
2022-07-24 09:13:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+const snes = require('./snes.json');
+
+const app = express();
+
+app.use(helmet());
+
+app.get('', (req, res) => {
+    res.json('Welcome to VG Static API');
+})
+
+app.get('/snes', (req, res) => {
+    res.json(snes);
+})
+
+const PORT = process.env.NODE_PORT || 3000;
+
+app.listen(PORT);
