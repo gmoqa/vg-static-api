6fc08fbbca5f4c8ba445c52c56ce1524b66d5845
Guillermo Quinteros <gmoqa@Guillermos-MacBook-Pro.local>
2022-07-24 15:54:07 -0400

    feat: add playstation titles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const snes = require('./SNES/all.json');
+const ps = require('./PS/all.json');
 
 const app = express();
 
@@ -13,6 +14,11 @@ app.use('/SNES', express.static(__dirname + '/SNES/all.json'));
 app.use('/SNES/USA', express.static(__dirname + '/SNES/usa.json'));
 app.use('/SNES/EUR', express.static(__dirname + '/SNES/eur.json'));
 app.use('/SNES/JAP', express.static(__dirname + '/SNES/jap.json'));
+
+app.use('/PS', express.static(__dirname + '/PS/all.json'));
+app.use('/PS/USA', express.static(__dirname + '/PS/usa.json'));
+app.use('/PS/EUR', express.static(__dirname + '/PS/eur.json'));
+app.use('/PS/JAP', express.static(__dirname + '/PS/jap.json'));
 
 app.get('', (req, res) => {
     res.json('Welcome to VG Static API');
