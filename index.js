a3f0a29698f6a7268d830d6ba91458cd8ac8a0f2
Guillermo Quinteros <gmoqa@Guillermos-MacBook-Pro.local>
2022-07-25 18:35:53 -0400

    feat: update home response

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const snes = require('./SNES/all.json');
-const ps = require('./PS/all.json');
-
 const app = express();
 
 app.use(helmet());
 app.use(morgan('tiny'));
+
+const url = 'https://vg-static-api.herokuapp.com';
 
 app.use('/SNES', express.static(__dirname + '/SNES/all.json'));
 app.use('/SNES/USA', express.static(__dirname + '/SNES/usa.json'));
@@ -21,7 +20,27 @@ app.use('/PS/EUR', express.static(__dirname + '/PS/eur.json'));
 app.use('/PS/JAP', express.static(__dirname + '/PS/jap.json'));
 
 app.get('', (req, res) => {
-    res.json('Welcome to VG Static API');
+    res.json({
+        message: 'Welcome to VG Static API',
+        services: [
+            {
+                snes: url + '/SNES',
+                regions: [
+                    url + '/SNES/USA',
+                    url + '/SNES/EUR',
+                    url + '/SNES/JAP',
+                ]
+            },
+            {
+                ps: url + '/PS',
+                regions: [
+                    url + '/PS/USA',
+                    url + '/PS/EUR',
+                    url + '/PS/JAP',
+                ]
+            }
+        ]
+    });
 })
 
 app.use((req, res, next) => {
